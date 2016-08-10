07fb8dec220b29ce9dc10e1325831558dd009938
Nayar Joolfoo <nayar@joolfoo.com>
2016-08-10 12:34:13 +0400

    search by domain

--- dashboard/index.js
+++ dashboard/index.js
@@ -7,24 +7,34 @@ var request = require('request');
 var bodyParser = require('body-parser');
 app.use(bodyParser.urlencoded({extended: true }));
 app.use(bodyParser.json());
-const exec = require('child_process').exec;;
+const exec = require('child_process').exec;
+
+var domains = []
 
 // respond with "hello world" when a GET request is made to the homepage
 app.get('/', function(req, res) {
     res.render('index.pug');
 });
 
+app.get('/api/domain', function(req, res) {
+    
+});
+
 app.get('/api/domain/_search', function(req, res) {
-    console.log(req)
+//     console.log(req)
     
     domain = req.query.domain
     
     cmd = 'echo | openssl s_client -connect ' + domain + ':443 -servername ' + domain + ' 2>/dev/null | openssl x509 -noout -enddate'
-    console.log(cmd)
+//     console.log(cmd)
     openssl = exec(cmd);
     
     openssl.stdout.on('data', function(data) {
-        res.send(data.trim());
+//         console.log(data)
+        data = data.trim()
+        date = data.split('=');
+        date = new Date(date[1])
+        res.send(date);
     }); 
     
     openssl.stderr.on('data', function(data) {
